3b8c8e4584c056575beaf98719a144354ffd0ad8
103AKSAM_GAMZE <103AKSAM_GAMZE@DESKTOP-TUMHS1A>
2021-11-23 18:23:17 +0300

    Add project files.

---
+++ MCD_MetodOdev/Matematik.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_MetodOdev
+{
+    class Matematik
+    {
+        public decimal Toplama(decimal sayi1 , decimal sayi2)
+        {
+            decimal sonuc = sayi1 + sayi2;
+            return sonuc;
+           
+
+        }
+         public decimal Cıkarma(decimal sayi1,decimal sayi2)
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+            
+        }
+        
+        public decimal Carpma(decimal sayi1,decimal sayi2)
+        {
+            decimal sonuc = sayi1 * sayi2;
+            return sonuc;
+            
+        }
+        
+        public decimal Bolme(decimal sayi1,decimal sayi2)
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+            
+        }
+
+        public void menuHazirla()
+        {
+            Console.Clear();
+            Console.WriteLine("****Menü****");
+            Console.WriteLine("1 - Toplama");
+            Console.WriteLine("2 - Çıkarma");
+            Console.WriteLine("3 - Çarpma");
+            Console.WriteLine("4 - Bölme");
+            Console.WriteLine("Lütfen bir değer seçiniz.");
+        }
+
+        public void sonucEkranaYaz(decimal Sayi1,decimal Sayi2,decimal sonuc,string operators)
+        {
+            Console.WriteLine("{0} {1} {2} = {3} ",Sayi1,operators,Sayi2,sonuc);
+        }
+    }
+}
---
+++ MCD_MetodOdev/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_MetodOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            /*
+             * Matematik adında bir class oluşturun.
+             * Bir menü hazırlayın metod ile
+             * Menüde
+             * "****Menü****"
+             * "1 - Toplama"
+             * "2 - Çıkartma"
+             * "3 - Çarpma"
+             * "4 - Bölme"
+             * "Lütfen bir değer seçiniz: "
+             * Metod ile 4 işlemleri ekrana yazdır metot ile yazdırılacak.
+             * public static void içinde matematik sınıfının bir instance'ini 
+             * al, kullanıcıdan iki değer iste
+             *ve matematik sınıfındaki 4 işlem metodunu kullanarak
+             *sonuçları ekrana yazdır.switch case olabilir.
+             *yeni işlem yapmak istiyor mu sor.(E/H)
+             *
+             */
+            Matematik m= new Matematik();
+            yenidenIslemYap:
+            m.menuHazirla();
+
+            
+            string secim = Console.ReadLine();
+            Console.WriteLine("Lütfen 1. sayıyı giriniz:");
+            decimal sayi1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Lütfen 2. sayıyı giriniz:");
+            decimal sayi2 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal sonuc = 0;
+
+            switch (secim)
+            {
+                case "1": //toplama
+                    sonuc = m.Toplama(sayi1, sayi2);
+                    m.sonucEkranaYaz(sayi1, sayi2, sonuc, "+");
+                    break;
+
+                case "2": //çıkarma
+                    sonuc = m.Cıkarma(sayi1, sayi2);
+                    m.sonucEkranaYaz(sayi1, sayi2, sonuc, "-");
+                    break;
+
+                case "3": //çarpma
+                    sonuc = m.Carpma(sayi1, sayi2);
+                    m.sonucEkranaYaz(sayi1, sayi2, sonuc, "*");
+                    break;
+
+                case "4": //toplama
+                    sonuc = m.Bolme(sayi1, sayi2);
+                    m.sonucEkranaYaz(sayi1, sayi2, sonuc, "/");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Belirtmiş olduğunuz değer liste içerisinde bulunamadı.");
+                    Console.WriteLine("Lütfen tekrar deneyiniz.");
+                    System.Threading.Thread.Sleep(2000);
+                    goto yenidenIslemYap;
+            }
+
+            Console.WriteLine("Yeni bir işlem yapmak istiyor musunuz? (E/H)");
+            string eh = Console.ReadLine();
+            if (eh=="e")
+            {
+                goto yenidenIslemYap;
+            }
+            
+
+
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
